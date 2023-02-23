5edae9fc5b0255f2e8ccd3f6cbc9aa881b3f45f9
Eduardo Soares <stremenx@gmail.com>
2023-02-23 20:40:24 -0300

    update 0.01
    Criação

---
+++ main.js
@@ -0,0 +1,61 @@
+function triangulo(){
+    var triA = Number(document.getElementById("inA").value);
+    var triB = Number(document.getElementById("inB").value);
+    var triC = Number(document.getElementById("inC").value);
+    var outInfo = document.getElementById("outInfo");
+    var outTriangulo = document.getElementById("outTriangulo");
+
+     
+    //########### Modelo Simplês ##############
+    // if(triA < (triB+triC) || triB < (triA+triC) || triC < (triA + triB)){
+    //     alert(`A Soma de "Um" alado não pode ser superior que a dos outros "Dois" somado`)
+    //     document.getElementById("inA").value = "",
+    //     inA.focus();
+    //     return;
+    // }
+
+    // Modelo completo com retorno a cedula errada
+    if(triA > (triB + triC)){
+        alert(`Erro! "A" não pode ser maior que assoma de "B" + "C"`);
+        document.getElementById("inA").value = "",
+        inA.focus();
+        return;} 
+    
+    if(triB > (triA + triC)){
+        alert(`Erro! "B" não pode ser maior que assoma de "A" + "C"`);
+        document.getElementById("inB").value = "",
+        inB.focus();
+        return;
+    } 
+    
+    if(triC > (triA + triB)){
+        alert(`Erro! "C" não pode ser maior que assoma de "A" + "B"`);
+        document.getElementById("inC").value = "",
+        inC.focus();
+        return;
+        }
+
+    // Triangulo: Equilátero
+    if(triA==triB && triB==triC){
+        var triangulo = "Equilátero"
+        outInfo.textContent=`Lados podem forma um triângulo.`;
+        outTriangulo.textContent=`Tipo: ${triangulo}`
+    }
+
+    // Triangulo: isóceles
+    if(triA==triB || triA==triC || triB==triC){
+        var triangulo = "Isóceles"
+        outInfo.textContent=`Lados podem forma um triângulo.`;
+        outTriangulo.textContent=`Tipo: ${triangulo}`
+    } 
+    
+    //Se não é as outras então é um triangulo: "Escaleno";
+    else {
+        var triangulo = "Escaleno"
+        outInfo.textContent=`Lados podem forma um triângulo.`;
+        outTriangulo.textContent=`Tipo: ${triangulo}`
+    } 
+}
+
+var btButton = document.getElementById("btButton");
+btButton.addEventListener("click", triangulo)
